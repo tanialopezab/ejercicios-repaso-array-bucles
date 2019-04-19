f7b0d92badf4b23f8c46934b465eae4d917c7234
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-19 12:54:14 +0200

    resolve exercice1

--- ejercicio7.1/main.js
+++ ejercicio7.1/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function workWithMovies (){
+const arrMovies = [
+    'star Wars',
+    'Invictus',
+    'E.T',
+];
+
+arrMovies[3] = "Vaiana";
+arrMovies[4] = "El Golpe";
+arrMovies[5] = "Cenicienta";
+
+console.log(arrMovies);
+
+arrMovies[2]= "El señor de los Anillos",
+arrMovies[3]= "Los goonies",
+
+console.log[arrMovies];
+
+}
+
+workWithMovies();//lo primero llamara la funcion, definirá el array inicial, añadira las peliculas, cambiará las posiciones [2][3]
