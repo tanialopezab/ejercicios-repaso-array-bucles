cd3307191889c0335a0ecb2e93ae776b98912324
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-19 17:21:03 +0200

    create files structure and first function of exercice 7

--- ejercicio7.7/main.js
+++ ejercicio7.7/main.js
@@ -1,1 +1,55 @@
 'use strict';
+
+// Objeto(elementos iguales ) que contiene array(elementos diferentes)
+const adalabers = [
+    
+    {
+        name: "María",
+        age: 29 + "años",
+        job: "diseñadora",
+    },
+
+    {
+        name: "Lucía",
+        age: 31 + "años",
+        job: "ingeniera quimica",
+    },
+
+    {
+        name: "Susana",
+        age: 34 + "años",
+        job: "periodista",
+    },
+
+    {
+        name: "Rocío",
+        age: 25 + "años",
+        job: "actriz",
+    },
+
+    {
+        name: "Inmaculada",
+        age: 21 + "años",
+        job: "diseñadora",
+    },
+
+]
+
+//FUNCION1
+//countAdalabers (número de adalabers del listado);
+
+function countAdalabers (arr) {
+    const numberMembers = arr.length
+    return console.log(numberMembers);
+}
+
+countAdalabers(adalabers);
+
+//FUNCION 2
+//averageAge que devuelve la media de edad de listado.
+
+function averageAge (age){
+
+}
+
+averageAge();
